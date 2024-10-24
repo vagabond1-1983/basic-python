193a8fbcd27ded95be50bd4f0d2ed52330f1ff38
vagabond1_1983 <vagabond1_1983@163.com>
2024-10-24 21:54:44 +0800

    feat: 网易云音乐爬虫完成

--- spider/Practices/WangyiMusic.py
+++ spider/Practices/WangyiMusic.py
@@ -1,6 +1,10 @@
 import requests
 import bs4
+import os
 
+filename = "music"
+if not os.path.exists(filename):
+    os.mkdir(filename)
 
 # 热歌榜地址
 url = "https://music.163.com/discover/toplist?id=3779629"
@@ -10,7 +14,25 @@ headers = {
 }
 
 resp = requests.get(url=url, headers=headers)
-print(resp)
+# print(resp.text)
 soup = bs4.BeautifulSoup(resp.text, "lxml")
-song_ids = soup.select("div > table")
-print(song_ids)
+song_ids = soup.css.select("div[data-key=song_toplist-3779629] ul li a:nth-of-type(1)")
+
+
+# 下载前5首歌曲
+for song in song_ids[:5]:
+    print(f"歌曲原始格式：{song}")
+    title = song.contents[0]
+    id = song["href"][song["href"].find("id=") : len(song["href"])]
+    music_download_url = f"http://music.163.com/song/media/outer/url?{id}.mp3"
+    print(f"音乐下载地址：{music_download_url}")
+    # todo 下载的 mp3 都是 195kb，有问题
+    resp = requests.get(url=music_download_url, headers=headers)
+    reditList = resp.history
+    print(f"获取最终重定向的headers头部信息：{reditList[len(reditList) - 1].headers}")
+    redirect_url = reditList[len(reditList) - 1].headers["location"]
+    print(f"获取重定向最终的url：{redirect_url}")
+    media_content = requests.get(url=redirect_url, headers=headers).content
+    with open(filename + os.path.sep + title + ".mp3", mode="wb") as f:
+        f.write(media_content)
+        print("歌曲下载完成\n============================")
